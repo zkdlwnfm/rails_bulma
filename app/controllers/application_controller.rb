80ab69f68043fd41ed1a71aa780111fd1fc6e709
박준호 <zkdlwnfm1@oozooinc.com>
2019-05-13 18:22:00 +0900

    [v0.1.0.0] 루트페이지 작업중

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def self.mobile_device?
+    if session[:mobile_param]
+      session[:mobile_param] == "1"
+    else
+      request.user_agent =~ /Mobile|webOS/
+    end
+  end
 end
