e29e80869dfabf8e89479c4f51c273fd36066f11
박준호 <zkdlwnfm1@oozooinc.com>
2019-05-14 18:49:55 +0900

    [v0.1.0.0] index 페이지 작업 중

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  helper_method :mobile_device?
 
-  def self.mobile_device?
+  def mobile_device?
     if session[:mobile_param]
       session[:mobile_param] == "1"
     else
-      request.user_agent =~ /Mobile|webOS/
+      request.user_agent =~ /Mobile/
     end
   end
 end
